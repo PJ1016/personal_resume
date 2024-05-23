3abec785a82bd07486a5467353acbd677985d37b
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2024-05-23 16:42:37 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./containers/home";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <Home
-          data={{
-            name: "Praveen Jayanth Kamatham",
-            mobileNumber: "9494039564",
-            emailAddress: "praveen.jayanth.1111@gmail.com",
-            address: "Hyderabad",
-            linkedInAddress: "www.linkedin.com/in/praveen-jayanth-8b0687199",
-          }}
-        />
-      ),
-    },
-  ]);
+  const isProduction = process.env.NODE_ENV === "production";
+
+  const basename = isProduction ? "/personal_resume" : "/";
+
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: (
+          <Home
+            data={{
+              name: "Praveen Jayanth Kamatham",
+              mobileNumber: "9494039564",
+              emailAddress: "praveen.jayanth.1111@gmail.com",
+              address: "Hyderabad",
+              linkedInAddress: "www.linkedin.com/in/praveen-jayanth-8b0687199",
+            }}
+          />
+        ),
+      },
+    ],
+    { basename }
+  );
 
   return <RouterProvider router={router} />;
 }
